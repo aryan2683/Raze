73f83564cbad9de830228a87d03d621367c926d5
QL-ARYAN-SINGHAL <aryan.singhal@quokkalabs.com>
2026-03-23 15:26:15 +0530

    First Commit

---
+++ Raze/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Raze
+//
+//  Created by KAKAROT on 3/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Raze/RazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RazeApp.swift
+//  Raze
+//
+//  Created by KAKAROT on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct RazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
